fc0e5c404b6dea6847fed1969e4123823774fea7
Devcorns Innovation <itprakhar@gmail.com>
2022-11-03 03:39:59 +0530

    update search & dropdown functionality

--- src/app/includes/front-header/front-header.component.ts
+++ src/app/includes/front-header/front-header.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ViewChild } from "@angular/core";
+
+import { NgbDropdown } from '@ng-bootstrap/ng-bootstrap';
+
+// import ext from "./../../json-apis/experience";
+import exp from './../../json-apis/experience.json';
+import jobprofile from './../../json-apis/jobprofiles.json';
 
 @Component({
     selector:'app-front-header',
@@ -7,12 +13,37 @@ import { Component, OnInit } from "@angular/core";
 })
 
  export class FrontHeaderComponent implements OnInit {
-    
+
+    expList:{id:number, title:string}[] = exp;
+    jobList:{id:number,title:string}[] = jobprofile;
+    experience_title:string="Experience";
+    location_title:string="Location";
+    jobtitle=""
+
+    @ViewChild('expDropdown', { static: true })
+    expdropdown!: NgbDropdown;
+    @ViewChild('locDropdown',{static: true})
+    locdropdown!:NgbDropdown;
     logo= "<Hire Me />"
 
-    ngOnInit() {
 
+     
+
+    ngOnInit() {
+        // console.log(this.countryList)
+        // this.expdropdown.open()    
+        // this.locdropdown.open()    
     }
+
+    getExp(val:string) {
+        this.experience_title = val
+    }
+    getLoc(val:string) {
+        this.location_title = val
+    }
+    searchHandle() {
+        console.log({exp:this.experience_title,loc:this.location_title,job:this.jobtitle})
+    }
+    
 }
 
-//  export  FrontHeader;
